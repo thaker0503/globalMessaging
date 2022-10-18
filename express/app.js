41bbf5fc52832f5740e687f2314eff351edd3544
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-10-18 19:02:47 +0530

    a

--- express/app.js
+++ express/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 const { initializeApp } = require("firebase/app");
-const { getAnalytics } = require("firebase/analytics");
+const { getDatabase, ref, set } = require("firebase/database");
 
 const app = express();
 
@@ -13,13 +14,25 @@ const firebaseConfig = {
     storageBucket: "global-chat-3ea31.appspot.com",
     messagingSenderId: "885110726689",
     appId: "1:885110726689:web:ef75cadb07fb6a64638360",
-    measurementId: "G-4H29YY356Z"
+    measurementId: "G-4H29YY356Z",
+    databaseURL: "https://global-chat-3ea31-default-rtdb.firebaseio.com/"
 };
 
 const app1 = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app1);
+// const database = getDatabase(app1);
 console.log(app1);
-console.log(analytics);
+// console.log(database);
+
+function writeUserData(userId, name, email, password) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+        name,
+        email,
+        password
+    })
+}
+
+// writeUserData(uuid(), 'yatharth', 'yatharth.thaker@technostacks.com', '123')
 
 app.use(express.json());
 app.set('view engine', 'ejs');
